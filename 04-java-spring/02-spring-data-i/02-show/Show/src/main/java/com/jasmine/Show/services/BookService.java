e1b2d97c58eb5c59c882d2a0de17dfd541df9378
Jasmine Regino <jsot24@gmail.com>
2020-10-28 22:06:42 -0500

    Show

---
+++ 04-java-spring/02-spring-data-i/02-show/Show/src/main/java/com/jasmine/Show/services/BookService.java
@@ -0,0 +1,39 @@
+package com.jasmine.Show.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jasmine.Show.models.Book;
+import com.jasmine.Show.repositories.BookRepository;
+
+@Service
+
+public class BookService {
+	private BookRepository bRepo;
+
+	public BookService(BookRepository repo) {
+		this.bRepo = repo;
+	}
+
+	public List<Book> getAllBooks() {
+		return this.bRepo.findAll();
+	}
+
+	public Book getOneBook(Long id) {
+		return this.bRepo.findById(id).orElse(null);
+	}
+
+	public Book createBook(Book newBook) {
+		return this.bRepo.save(newBook);
+	}
+
+	public Book updateBook(Book updatedBook) {
+		return this.bRepo.save(updatedBook);
+	}
+
+	public String deleteBook(Long id) {
+		this.bRepo.deleteById(id);
+		return "Book" + id + "has been deleted";
+	}
+}
---
+++ 04-java-spring/02-spring-data-i/02-show/Show/src/test/java/com/jasmine/Show/ShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasmine.Show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
