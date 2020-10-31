fd7d037afbc4b0bfe868c270ec0a4634087ae1df
Jasmine Regino <jsot24@gmail.com>
2020-10-31 11:19:32 -0500

    lookify

---
+++ 04-java-spring/02-spring-data-i/04-lookify/lookify/src/main/java/com/jasmine/lookify/repositories/SongRepository.java
@@ -0,0 +1,14 @@
+package com.jasmine.lookify.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jasmine.lookify.models.Song;
+
+public interface SongRepository extends CrudRepository<Song, Long>{
+    List<Song> findAll();
+    List<Song> findTop10ByOrderByRatingDesc();
+    List<Song> findByArtistContaining(String artist);
+}
+
